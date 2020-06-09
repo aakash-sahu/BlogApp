e41b74f46bda537f1203cfe3ae4078053b224ed6
coolakss <coolakss@yahoo.com>
2020-06-08 21:47:47 -0400

    template and jinja basics

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': "Jan 1, 2020"
+    },
+        {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': "Jan 2, 2020"
+    },
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts) # template will have access to posts variable
 
 @app.route("/about")
 def about():
-    return "<h1>About page</>"
+    return render_template('about.html', title = 'About')
 
 if __name__ == "__main__":
     app.run(debug=True)
