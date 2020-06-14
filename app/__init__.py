9cbd90e87a0e4c63a0ac44d110b878bd2ac89059
coolakss <coolakss@yahoo.com>
2020-06-14 19:15:36 -0400

    added account button and page for authenticated user

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #/// means relative 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login' # set as function name of login route
 
 # Routes need to be imported so app can find them when running the application
 # Put routes import here to avoid circular import??
