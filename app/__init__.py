0c798bd630e37657e59d30e8610d4b9fc2050942
coolakss <coolakss@yahoo.com>
2020-06-14 14:52:50 -0400

    password hash and user registration

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -7,6 +8,7 @@ app.config['SECRET_KEY'] = '656dd199eb5c5c86d449ae28f3d0fcc8'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #/// means relative path
 # set up db with app
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 # Routes need to be imported so app can find them when running the application
 # Put routes import here to avoid circular import??
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from app import app
+from app import app, db, bcrypt
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Post
 
@@ -31,10 +31,14 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account create for {form.username.data}!', 'success') #on POST form calls back the same method and loads up the values as .data attribute
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email = form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created. You are now able to log in', 'success') #on POST form calls back the same method and loads up the values as .data attribute
         # 'success' is from bootstrap, flash can take this as argument.
         # next redirect to home page on success
-        return redirect(url_for('home')) ## redirect every link is the name of function
+        return redirect(url_for('login')) ## redirect every link is the name of function
     return render_template('register.html', title='Register', form = form)
 
 @app.route("/login", methods=['GET', 'POST'])
