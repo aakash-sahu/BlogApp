8e4b66cf39d1b63242031e0e7f8d052e13ec1b89
coolakss <coolakss@yahoo.com>
2020-06-21 22:00:10 -0400

    resize uploaded pics using pillow

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 import secrets, os
+from PIL import Image
 
 posts = [
     {
@@ -76,7 +77,11 @@ def save_picture(form_picture):
     picture_fn = random_hex + f_ext
     # add picture to root path of application + profile photo folder
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+    ##resize picuture before saving to save space on file system and increase load speed of page
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
     # return file name of picture
     return picture_fn
 
