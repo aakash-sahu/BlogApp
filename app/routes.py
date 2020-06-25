75b55b1b1d41ec4f4f5bbf30618819cde6cf1836
coolakss <coolakss@yahoo.com>
2020-06-25 16:41:05 -0400

    added posts database connection

--- app/routes.py
+++ app/routes.py
@@ -6,24 +6,11 @@ from flask_login import login_user, current_user, logout_user, login_required
 import secrets, os
 from PIL import Image
 
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': "Jan 1, 2020"
-    },
-        {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': "Jan 2, 2020"
-    },
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts) # template will have access to posts variable
 
 @app.route("/about")
@@ -116,6 +103,9 @@ def account():
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post', form=form)
