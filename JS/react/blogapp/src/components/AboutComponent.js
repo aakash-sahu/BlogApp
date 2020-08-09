5e9fe6b0bfa07bafbbd8f7e2a3be07a7384b8c96
coolakss <coolakss@yahoo.com>
2020-08-09 17:00:28 -0400

    added architecture on about page

--- JS/react/blogapp/src/components/AboutComponent.js
+++ JS/react/blogapp/src/components/AboutComponent.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 function About(props)  {
     return (
-        <div>About Page</div>
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-md-8 offset-md-2">
+                    <h3>Architecture of the app</h3>
+                    <img style={{width:"100%", margin:"2em 0 0 0", border: "1px solid grey", padding:"5px"}} src="/Architecture.jpg" />
+                </div>
+            </div>
+
+        </div>
     );
 }
 
